6f7cf1a254b403365f88bd29417ba2dc7e3a9934
S Perera <sperera1922@gmail.com>
2026-07-03 23:26:09 +0530

    Implement user menu and connect all student operations

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,45 @@
-#include <iostream>
-using namespace std;
+#include "Student.h"
 
 int main() {
-    cout << "Student Management System" << endl;
+
+    int choice;
+
+    loadFromFile();
+    
+    while(true){
+        cout << "===== Student Management System =====" << endl;
+        cout << "\n1. Add Student" << endl;
+        cout << "2. View Students" << endl;
+        cout << "3. Search Student" << endl;
+        cout << "4. Delete Student" << endl;
+        cout << "5. Update student" << endl;
+        cout << "6. Exit\n" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch(choice){
+            case 1:
+                addStudent();
+                break;
+            case 2:
+                viewStudents();
+                break;
+            case 3:
+                searchStudent();
+                break;
+            case 4:
+                deleteStudent();
+                break;
+            case 5:
+                updateStudent();
+                break;
+            case 6:
+                cout << "Exiting..." << endl;
+                return 0;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    }
+
     return 0;
 }
